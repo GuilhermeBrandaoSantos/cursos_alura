1579ddf9050fae9261f19db1a70d5194c51e3b90
GuilhermeBrandaoSantos <santos.gb01@outlook.com>
2020-02-15 19:06:25 -0300

    Criando validações dentro do construtor utilizando método privado

--- php/orientacao_objeto_php/classes/Conta.php
+++ php/orientacao_objeto_php/classes/Conta.php
@@ -1,59 +1,72 @@
 <?php
-
-class Conta
-{
-    private $cpf;
-    private $nome_titular;
-    private $saldo = 0;
-
-
-    public function __construct(string $cpf_cliente, string $nome_titular_novo)
+    class Conta
     {
-        $this->cpf = $cpf_cliente;
-        $this->nome_titular = $nome_titular_novo;
-        $this->saldo = 0;
-    }    
+        /************ Atributos ************/
 
-    public function sacar(float $valor_sacar)
-    {
-        if ($valor_sacar > $this->saldo) {
-            echo 'Saque não autorizado! <br>';
-            echo 'Valor do saque é maior que o saldo disponível!';
+        private $cpf;
+        private $nome_titular;
+        private $saldo = 0;
 
-            return;
-        }
-        
-        $this->saldo -= $valor_sacar;
-        echo '<br><b>'.$this->nome_titular.'</b><br>' .'Seu saldo autal é de R$' . $this->saldo;
-                    
-    }
+        /************ Métodos públicos ************/
 
-    public function depositar(float $valor_depositar):void
-    {
-        if ($valor_depositar < 0) {
-            echo 'Depósito não autorizado! <br>';
-            echo 'Valor do depósito é menor que 0. Deposite um valor válido!';
+        public function __construct(string $cpf_cliente, string $nome_titular_novo)
+        {
+            $this->cpf = $cpf_cliente;
+            $this->validaNome($nome_titular_novo);
+            $this->nome_titular = $nome_titular_novo;
+            $this->saldo = 0;
+        }    
 
-            return;
-        } 
-        
-        $this->saldo += $valor_depositar;
-        echo '<br><b>'.$this->nome_titular.'</b><br>' .'Seu saldo autal é de R$' . $this->saldo;
-        
-    }
+        public function sacar(float $valor_sacar)
+        {
+            if ($valor_sacar > $this->saldo) {
+                echo 'Saque não autorizado! <br>';
+                echo 'Valor do saque é maior que o saldo disponível!';
 
-    public function transferir(float $valor_transferir, $conta_tranferir):void
-    {
-        if ($valor_transferir > $this->saldo) {
-            echo 'Transferência não autorizada! <br>';
-            echo 'Valor da transferência é maior que o saldo disponível!';
-
-            return;
+                return;
+            }
+            
+            $this->saldo -= $valor_sacar;
+            echo '<br><b>'.$this->nome_titular.'</b><br>' .'Seu saldo autal é de R$' . $this->saldo;
+                        
         }
 
-        $this->sacar($valor_transferir);
-        $conta_tranferir->depositar($valor_transferir);
+        public function depositar(float $valor_depositar):void
+        {
+            if ($valor_depositar < 0) {
+                echo 'Depósito não autorizado! <br>';
+                echo 'Valor do depósito é menor que 0. Deposite um valor válido!';
+
+                return;
+            } 
+            
+            $this->saldo += $valor_depositar;
+            echo '<br><b>'.$this->nome_titular.'</b><br>' .'Seu saldo autal é de R$' . $this->saldo;
+            
+        }
+
+        public function transferir(float $valor_transferir, $conta_tranferir):void
+        {
+            if ($valor_transferir > $this->saldo) {
+                echo 'Transferência não autorizada! <br>';
+                echo 'Valor da transferência é maior que o saldo disponível!';
+
+                return;
+            }
+
+            $this->sacar($valor_transferir);
+            $conta_tranferir->depositar($valor_transferir);
+
+        }
+
+        /************ Métodos privados ************/
+
+        private function validaNome($nome_titular)
+        {
+            if (strlen($nome_titular) < 5) {
+                echo 'Nome inválido. O nome precisa conter no mínimo 5 caracteres'; 
+                exit();
+            }
+        }
 
     }
-
-}
