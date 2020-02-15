8282e4b3fdf7c01324bad6472c2afb9a8ddeb8e8
GuilhermeBrandaoSantos <santos.gb01@outlook.com>
2020-02-15 13:48:24 -0300

    Definindo e utilizando metodos

--- php/orientacao_objeto_php/classes/Conta.php
+++ php/orientacao_objeto_php/classes/Conta.php
@@ -1,7 +1,51 @@
 <?php
 
-class Conta{
+class Conta
+{
     public $cpf;
     public $nome_titular;
-    public $saldo;
+    public $saldo = 0;
+
+    public function sacar(float $valor_sacar)
+    {
+        if ($valor_sacar > $this->saldo) {
+            echo 'Saque não autorizado! <br>';
+            echo 'Valor do saque é maior que o saldo disponível!';
+
+            return;
+        }
+        
+        $this->saldo -= $valor_sacar;
+        echo '<br><b>'.$this->nome_titular.'</b><br>' .'Seu saldo autal é de R$' . $this->saldo;
+                    
+    }
+
+    public function depositar(float $valor_depositar):void
+    {
+        if ($valor_depositar < 0) {
+            echo 'Depósito não autorizado! <br>';
+            echo 'Valor do depósito é menor que 0. Deposite um valor válido!';
+
+            return;
+        } 
+        
+        $this->saldo += $valor_depositar;
+        echo '<br><b>'.$this->nome_titular.'</b><br>' .'Seu saldo autal é de R$' . $this->saldo;
+        
+    }
+
+    public function transferir(float $valor_transferir, $conta_tranferir):void
+    {
+        if ($valor_transferir > $this->saldo) {
+            echo 'Transferência não autorizada! <br>';
+            echo 'Valor da transferência é maior que o saldo disponível!';
+
+            return;
+        }
+
+        $this->sacar($valor_transferir);
+        $conta_tranferir->depositar($valor_transferir);
+
+    }
+
 }
--- php/orientacao_objeto_php/src/index.php
+++ php/orientacao_objeto_php/src/index.php
@@ -9,10 +9,17 @@ $segunda_conta = new Conta();
 
 $primeria_conta->cpf = '123.456.789-10';
 $primeria_conta->nome_titular = 'Guilherme Brandão';
-$primeria_conta->saldo = 1500;
+$primeria_conta->saldo = 15500.00;
+
+// $primeria_conta->sacar(500);
+$primeria_conta->depositar(500);
 
 $segunda_conta->cpf = '109.876.543-21';
 $segunda_conta->nome_titular = 'Satialis Anjo';
-$segunda_conta->saldo = 2654;
+$segunda_conta->saldo = 4654.55;
+
+// $segunda_conta->sacar(100);
+$segunda_conta->depositar(1000);
+
 
 var_dump($primeria_conta, $segunda_conta);
