e6c0e75c7e06d275040ddeecb38c61f3e61a85ba
GuilhermeBrandaoSantos <santos.gb01@outlook.com>
2020-03-19 12:03:41 -0300

    Atualização dos arquivos

--- php/avancando_oo/autoload.php
+++ php/avancando_oo/autoload.php
@@ -1,11 +1,11 @@
 <?php
-    spl_autoload_register(function(string $nome_completo_classe){
 
-        $caminho_arquivo = str_replace('Cursos_alura\\POO', 'src', $nome_completo_classe);
-        $caminho_arquivo = str_replace('\\', DIRECTORY_SEPARATOR, $caminho_arquivo);
-        $caminho_arquivo .= '.php';
+spl_autoload_register(function (string $nomeCompletoDaClasse) {
+    $caminhoArquivo = str_replace('Alura\\Banco', 'src', $nomeCompletoDaClasse);
+    $caminhoArquivo = str_replace('\\', DIRECTORY_SEPARATOR, $caminhoArquivo);
+    $caminhoArquivo .= '.php';
 
-        if (file_exists($caminho_arquivo)) {
-            require_once $caminho_arquivo;
-        }
-    });
+    if (file_exists($caminhoArquivo)) {
+        require_once $caminhoArquivo;
+    }
+});
