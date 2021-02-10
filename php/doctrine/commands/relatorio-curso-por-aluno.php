5759fa77803a321ecbd1c2e34f2193e4dfc96431
Guilherme Brandão dos Santos <santos.gb01@outlook.com>
2021-02-10 13:14:04 -0300

    add relatorio dos cursos por aluno and add debug stack

---
+++ php/doctrine/commands/relatorio-curso-por-aluno.php
@@ -0,0 +1,41 @@
+<?php
+
+use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Entity\Telefone;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+use Doctrine\DBAL\Logging\DebugStack;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+$alunoRepository = $entityManager->getRepository(Aluno::class);
+
+//fazendo debug do que esta sendo executado
+$debugStack = new DebugStack();
+$entityManager->getConfiguration()->setSQLLogger($debugStack);
+
+$alunos = $alunoRepository->findAll();
+
+foreach ($alunos as $aluno) {
+
+   $telefones = $aluno
+      ->getTelefones()
+      ->map(function (Telefone $telefone){
+         return $telefone->getNumero();
+      })->toArray()
+   ;
+   echo "ID: {$aluno->getId()}\n";
+   echo "Nome: {$aluno->getNome()}\n";
+   echo "Telefones: " . implode(',' , $telefones). "\n";
+
+   $cursos = $aluno->getCurso();
+   foreach ($cursos as $curso) {
+      echo "\tID Curso: {$curso->getId()}\n";
+      echo "\tCurso: {$curso->getNome()}\n";
+   }
+   echo "\n";
+}
+
+print_r($debugStack);
+
